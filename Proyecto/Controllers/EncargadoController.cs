488ac4a7e9837b2f6ce322d84544cc7c75e99f1a
Geison Eduardo <gvillalobos40174@ufide.ac.cr>
2021-04-10 10:48:42 -0600

    1. Creacion de controladores de encargado y empleado, 2. Migracion inicial de base de datos

--- Proyecto/Controllers/EncargadoController.cs
+++ Proyecto/Controllers/EncargadoController.cs
@@ -2,31 +2,34 @@
 using Microsoft.EntityFrameworkCore;
 using ProyectoMvc.DataAccess.Repositorio.IRepositorio;
 using ProyectoMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace ProyectoMvc.Controllers
 {
-    public class NinoController : Controller
+    public class EncargadoController : Controller
     {
-        public NinoController(IControlador controlador)
+        public EncargadoController(IControlador controlador)
         {
             _controlador = controlador;
         }
-
         readonly IControlador _controlador;
 
-        public IActionResult Nino()
+        public IActionResult Encargado()
         {
             return View();
         }
 
         [HttpGet]
-        public IActionResult NinoUpsert(int id = 0)
+        public IActionResult EncargadoUpsert(int id = 0)
         {
             if (id == 0)
-                return View(new Nino());
+                return View(new Encargado());
             else
             {
-                var t = _controlador.Ninos.Buscar(id);
+                var t = _controlador.Encargados.Buscar(id);
                 if (t == null)
                 {
                     return NotFound();
@@ -37,25 +40,25 @@ namespace ProyectoMvc.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult NinoUpsert(int id, Nino nino)
+        public IActionResult EncargadoUpsert(int id, Encargado encargado)
         {
             if (ModelState.IsValid)
             {
                 if (id == 0)
                 {
-                    _controlador.Ninos.Agregar(nino);
+                    _controlador.Encargados.Agregar(encargado);
                     _controlador.Guardar();
                 }
                 else
                 {
                     try
                     {
-                        _controlador.Ninos.Actualizar(nino);
+                        _controlador.Encargados.Actualizar(encargado);
                         _controlador.Guardar();
                     }
                     catch (DbUpdateConcurrencyException)
                     {
-                        if (_controlador.Ninos.Buscar(nino.Id) == null)
+                        if (_controlador.Encargados.Buscar(encargado.Id) == null)
                         {
                             return NotFound();
                         }
@@ -66,7 +69,7 @@ namespace ProyectoMvc.Controllers
                     }
                 }
 
-                return Json(new { success = true, message = "El nuevo ingreso ha sido guardada." });
+                return Json(new { success = true, message = "El registro se ha guardado exitosamente." });
             }
 
             return Json(new { success = false, message = "Ocurrió un error guardando el nuevo ingreso." });
@@ -75,21 +78,20 @@ namespace ProyectoMvc.Controllers
         [HttpGet]
         public IActionResult Listar()
         {
-            return Json(new { success = true, data = _controlador.Ninos.Listar() });
+            return Json(new { success = true, data = _controlador.Encargados.Listar() });
         }
 
         [HttpDelete]
         public IActionResult Borrar(int id)
         {
-            var t = _controlador.Ninos.Buscar(id);
+            var t = _controlador.Encargados.Buscar(id);
             if (t == null)
             {
-                return Json(new { success = false, message = "Niño o niña no borrado." });
+                return Json(new { success = false, message = "El registro no ha sido borrado." });
             }
-            _controlador.Ninos.Remover(t);
+            _controlador.Encargados.Remover(t);
             _controlador.Guardar();
-            return Json(new { success = true, message = "El niño ha sido borrado." });
+            return Json(new { success = true, message = "Registro borrado con éxito." });
         }
     }
 }
-
