40f922940717c7049066e3a1b3e23f8465505960
eri1200 <60492573+eri1200@users.noreply.github.com>
2021-04-20 20:39:23 -0600

    Upsert vista

---
+++ ProyectoMvc.DataAccess/Repositorio/IRepositorio/IUnidadTrabajo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoMvc.DataAccess.Repositorio.IRepositorio
+{
+    public interface IUnidadTrabajo : IDisposable
+    {
+        IBitacoraRepositorio Bitacoras { get; }
+
+        INinoRepositorio Ninos { get; }
+
+        IEmpleadoRepositorio Empleados { get; }
+
+        ITipoIdentificacionRepositorio TiposIdentificacion { get; }
+
+        IProcedimientoAlmacenado ProcedimientoAlmacenado { get; }
+
+        void Guardar();
+    }
+}
